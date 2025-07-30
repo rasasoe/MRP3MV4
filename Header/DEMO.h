5b4c609b7ad86610de6f805e855fb658f1f5dd06
rasasoe <kjkoly447@gmail.com>
2025-07-30 11:30:37 +0900

    Create DEMO.h

---
+++ Header/DEMO.h
@@ -0,0 +1,23 @@
+#ifndef _Demo_H
+#define _Demo_H
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void Demo_main(void);
+void DEMO1(void);
+void DEMO2(void);
+void DEMO3(void);
+void DEMO4(void);
+void DEMO5(void);
+void DEMO6(void);
+void DEMO7(void);
+
+void TASK1(void);
+void TASK2(void);
+void TASK3(void);
+void TASK4(void);
+void TASK5(void);
+void TASK6(void);
+
+#endif
